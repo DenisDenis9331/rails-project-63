05d04eb6af72293e4e45a15bdf1ccb0e74b7322b
denis <denisdenis9331@gmail.com>
2023-01-13 23:47:56 +0500

    feature: add test, generate user and method form_for

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,4 +5,10 @@ require 'hexlet_code/version'
 # This module is used to generate HTML forms
 module HexletCode
   autoload(:Tag, 'tag')
+
+  User = Struct.new(:name, :job, keyword_init: true)
+
+  def self.form_for(user, url: '#')
+    Tag.build('form', action: url, method: 'post')
+  end
 end
--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -6,4 +6,18 @@ class HexletCodeTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
+
+  def init_user
+    @user = User.new(name: 'rob', job: 'hexlet')
+  end
+  def test_form_for_with_url
+    form = HexletCode.form_for @user, url: '/users' do |f|
+    end
+    assert_equal '<form action="/users" method="post"></form>', form
+  end
+  def test_form_for_without_url
+    form = HexletCode.form_for @user do |f|
+    end
+    assert_equal '<form action="#" method="post"></form>', form
+  end
 end
